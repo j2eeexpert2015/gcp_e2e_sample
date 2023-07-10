e6354dc19aecc191ee6870e7b871ef688d4d3bc6
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2023-07-10 23:37:29 +0530

    Added changes

---
+++ src/main/resources/composer/dags/invoke_bq_sp_daily_sales_summary.py
@@ -0,0 +1,39 @@
+import datetime
+import time
+
+import datetime
+
+from airflow import models
+from airflow.providers.google.cloud.operators.bigquery import (
+    BigQueryInsertJobOperator,
+    BigQueryIntervalCheckOperator,
+    BigQueryValueCheckOperator,
+)
+
+yesterday = datetime.datetime.combine(
+    datetime.datetime.today() - datetime.timedelta(1),
+    datetime.datetime.min.time())
+
+default_dag_args = {
+    'start_date': yesterday
+}
+
+location="us-central1"
+
+with models.DAG(
+        'call generate_daily_sales_summary',
+        schedule_interval=datetime.timedelta(days=1),
+        default_args=default_dag_args) as dag:
+        
+    call_stored_procedure = BigQueryInsertJobOperator(
+    task_id="call_generate_daily_sales_summary",
+    configuration={
+        "query": {
+            "query": "CALL `poised-shuttle-384406.gcpsample.generate_daily_sales_summary`('2023-07-01') ",
+            "useLegacySql": False,
+        }
+    },
+    location=location,
+)
+
+    call_stored_procedure
