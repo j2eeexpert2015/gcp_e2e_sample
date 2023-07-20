d7e82e66fa7c73f578e3953d42489a77a62f5aae
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2023-07-20 23:02:52 +0530

    Added changes

---
+++ src/main/java/gcpsample/GCPConstants.java
@@ -0,0 +1,10 @@
+package gcpsample;
+
+public interface GCPConstants {
+    String PROJECT_ID = "sanguine-anthem-393416";
+    String DATASET_ID="gcpsample";
+    String DATAFLOW_GCS_BUCKET_NAME="gcpsampleall";
+    String DATAFLOW_TEMP_FOLDER="gcpsample";
+
+    String GBQ_SOURCE_TABLE_NAME="dvcl";
+}
